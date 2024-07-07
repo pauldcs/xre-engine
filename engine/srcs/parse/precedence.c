b28962eb002888938c9fb1470380851d50148e55
pducos <pducos@student.42.fr>
2024-07-07 19:52:24 +0200

    fix(runtime): fix do operator not being chainable

--- engine/srcs/parse/precedence.c
+++ engine/srcs/parse/precedence.c
@@ -3,26 +3,27 @@
 
 int get_precedence_by_kind(xre_expr_kind_t kind)
 {
+	// if (kind == __BUILTIN_CALL__) {
+	// 	switch (expr_type_by_kind(kind)) {
+	// 		case EXPR_OP_TYPE_BINOP:
+	// 			goto builtin_binop;
+
+	// 		case EXPR_OP_TYPE_UNIOP:
+	// 			goto uniop;
+
+	// 		default:
+	// 			goto prison;
+	// 	}
+	// }
+
 	switch (kind) {
-	case __BUILTIN_CALL__:
-		switch (expr_type_by_kind(kind)) {
-		case EXPR_OP_TYPE_BINOP:
-			goto conditional;
-
-		case EXPR_OP_TYPE_UNIOP:
-			goto uniop;
-
-		default:
-			goto prison;
-		}
-
 	case __SCOPE_RESOLUTION__:
 	case __START__:
 	case __END__:
 		return (0);
 
-uniop:
 	case __NOT__:
+	case __BUILTIN_CALL__:
 		return (-1);
 
 	case __POW__:
@@ -45,39 +46,44 @@ uniop:
 	case __GT__:
 	case __LE__:
 	case __GE__:
-		return (-8);
+		return (-7);
 
 	case __EQ__:
 	case __NE__:
-		return (-9);
+		return (-8);
 
 	case __BAND__:
-		return (-10);
+		return (-9);
 
 	case __BXOR__:
-		return (-11);
+		return (-10);
 
 	case __BOR__:
-		return (-12);
+		return (-11);
 
-conditional:
 	case __AND__:
-	case __DO__:
-	case __OR__:
-	case __ELSE__:
 		return (-13);
 
-	case __LOOP__:
+	case __OR__:
+		return (-14);
+
+	case __DO__:
 		return (-15);
 
-	case __SEQUENCE__:
+	case __ELSE__:
 		return (-16);
 
-	case __ASSIGN__:
+	case __LOOP__:
+		return (-17);
+
+	case __SEQUENCE__:
 		return (-18);
 
-	case __SEPARATOR__:
+	case __ASSIGN__:
 		return (-19);
+
+	case __SEPARATOR__:
+		return (-20);
 
 	case __LPAREN__:
 	case __RPAREN__:
@@ -86,10 +92,11 @@ conditional:
 	case __VAL__:
 	case __STRING_LITERAL__:
 	case __VARIABLE__:
+	default:
+
 		break;
 	}
 
-prison:
 	__return_val_if_fail__(false, 0);
 	__builtin_unreachable();
 }
